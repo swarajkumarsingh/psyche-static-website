557e68b8f65b69a9922336f053a761f36f45f9f9
swarajkumarsingh <sswaraj169@gmail.com>
2025-02-10 14:20:51 +0530

    fix: dark mode off

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("scale-y-0");
   mobileMenu.classList.toggle("scale-y-100");
 });
+
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  document.querySelector("nav").classList.add("bg-gray-800");
+} else {
+  document.querySelector("nav").classList.add("bg-white");
+}
